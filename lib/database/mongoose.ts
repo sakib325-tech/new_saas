9a5c607999d6854f93de11cd5e6cf9ee305b3f4b
sakib325-tech <darkzonebd123@gmail.com>
2025-01-19 02:11:55 +0600

    Update mongoose.ts

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,37 +1,37 @@
-import mongoose, {Mongoose} from "mongoose";
+// import mongoose, {Mongoose} from "mongoose";
 
 
 
-const MONGODB_URL = process.env.MONGODB_URL;
+// const MONGODB_URL = process.env.MONGODB_URL;
 
-interface MongooseConnection {
+// interface MongooseConnection {
 
-   conn: Mongoose | null; 
-   promise: Promise<Mongoose> | null;
+//    conn: Mongoose | null; 
+//    promise: Promise<Mongoose> | null;
 
-}
-
-
-let cached: MongooseConnection = (global as any).mongoose 
+// }
 
 
-if(!cached) {
-    cached = (global as any).mongoose = {conn: null,
-        promise: null
-    }
-}
+// let cached: MongooseConnection = (global as any).mongoose 
 
 
-export const connectToDatabase = async () => {
-    if(cached.conn) return cached.conn;
+// if(!cached) {
+//     cached = (global as any).mongoose = {conn: null,
+//         promise: null
+//     }
+// }
 
-    if(!MONGODB_URL) throw new Error("MONGODB_URL is not defined");
 
-    cached.promise = cached.promise || mongoose.connect (MONGODB_URL, {
-        dbName: "velora", bufferCommands: false
-    })
+// export const connectToDatabase = async () => {
+//     if(cached.conn) return cached.conn;
 
-    cached.conn = await cached.promise;
-    return cached.conn;
+//     if(!MONGODB_URL) throw new Error("MONGODB_URL is not defined");
+
+//     cached.promise = cached.promise || mongoose.connect (MONGODB_URL, {
+//         dbName: "velora", bufferCommands: false
+//     })
+
+//     cached.conn = await cached.promise;
+//     return cached.conn;
     
-}
+// }
