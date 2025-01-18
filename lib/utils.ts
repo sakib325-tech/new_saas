00f4f463689038b869e574930dc8b16e15b96c19
sakib325-tech <darkzonebd123@gmail.com>
2025-01-19 02:12:38 +0600

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,162 +3,162 @@
 
 // export function cn(...inputs: ClassValue[]) {
 //   return twMerge(clsx(inputs))
+ // }
+
+// /* eslint-disable prefer-const */
+// /* eslint-disable no-prototype-builtins */
+// import { type ClassValue, clsx } from "clsx";
+// import qs from "qs";
+// import { twMerge } from "tailwind-merge";
+
+// import { aspectRatioOptions } from "@/constants";
+
+// export function cn(...inputs: ClassValue[]) {
+//   return twMerge(clsx(inputs));
 // }
 
-/* eslint-disable prefer-const */
-/* eslint-disable no-prototype-builtins */
-import { type ClassValue, clsx } from "clsx";
-import qs from "qs";
-import { twMerge } from "tailwind-merge";
+// // ERROR HANDLER
+// export const handleError = (error: unknown) => {
+//   if (error instanceof Error) {
+//     // This is a native JavaScript error (e.g., TypeError, RangeError)
+//     console.error(error.message);
+//     throw new Error(`Error: ${error.message}`);
+//   } else if (typeof error === "string") {
+//     // This is a string error message
+//     console.error(error);
+//     throw new Error(`Error: ${error}`);
+//   } else {
+//     // This is an unknown type of error
+//     console.error(error);
+//     throw new Error(`Unknown error: ${JSON.stringify(error)}`);
+//   }
+// };
 
-import { aspectRatioOptions } from "@/constants";
+// // PLACEHOLDER LOADER - while image is transforming
+// const shimmer = (w: number, h: number) => `
+// <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+//   <defs>
+//     <linearGradient id="g">
+//       <stop stop-color="#7986AC" offset="20%" />
+//       <stop stop-color="#68769e" offset="50%" />
+//       <stop stop-color="#7986AC" offset="70%" />
+//     </linearGradient>
+//   </defs>
+//   <rect width="${w}" height="${h}" fill="#7986AC" />
+//   <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+//   <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+// </svg>`;
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
+// const toBase64 = (str: string) =>
+//   typeof window === "undefined"
+//     ? Buffer.from(str).toString("base64")
+//     : window.btoa(str);
 
-// ERROR HANDLER
-export const handleError = (error: unknown) => {
-  if (error instanceof Error) {
-    // This is a native JavaScript error (e.g., TypeError, RangeError)
-    console.error(error.message);
-    throw new Error(`Error: ${error.message}`);
-  } else if (typeof error === "string") {
-    // This is a string error message
-    console.error(error);
-    throw new Error(`Error: ${error}`);
-  } else {
-    // This is an unknown type of error
-    console.error(error);
-    throw new Error(`Unknown error: ${JSON.stringify(error)}`);
-  }
-};
+// export const dataUrl = `data:image/svg+xml;base64,${toBase64(
+//   shimmer(1000, 1000)
+// )}`;
+// // ==== End
 
-// PLACEHOLDER LOADER - while image is transforming
-const shimmer = (w: number, h: number) => `
-<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <defs>
-    <linearGradient id="g">
-      <stop stop-color="#7986AC" offset="20%" />
-      <stop stop-color="#68769e" offset="50%" />
-      <stop stop-color="#7986AC" offset="70%" />
-    </linearGradient>
-  </defs>
-  <rect width="${w}" height="${h}" fill="#7986AC" />
-  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-</svg>`;
+// // FORM URL QUERY
+// export const formUrlQuery = ({
+//   searchParams,
+//   key,
+//   value,
+// }: FormUrlQueryParams) => { 
+//   const params = { ...qs.parse(searchParams.toString()), [key]: value };
 
-const toBase64 = (str: string) =>
-  typeof window === "undefined"
-    ? Buffer.from(str).toString("base64")
-    : window.btoa(str);
+//   return `${window.location.pathname}?${qs.stringify(params, {
+//     skipNulls: true,
+//   })}`;
+// };
 
-export const dataUrl = `data:image/svg+xml;base64,${toBase64(
-  shimmer(1000, 1000)
-)}`;
-// ==== End
+// // REMOVE KEY FROM QUERY
+// export function removeKeysFromQuery({
+//   searchParams,
+//   keysToRemove,
+// }: RemoveUrlQueryParams) {
+//   const currentUrl = qs.parse(searchParams);
 
-// FORM URL QUERY
-export const formUrlQuery = ({
-  searchParams,
-  key,
-  value,
-}: FormUrlQueryParams) => { 
-  const params = { ...qs.parse(searchParams.toString()), [key]: value };
+//   keysToRemove.forEach((key) => {
+//     delete currentUrl[key];
+//   });
 
-  return `${window.location.pathname}?${qs.stringify(params, {
-    skipNulls: true,
-  })}`;
-};
+//   // Remove null or undefined values
+//   Object.keys(currentUrl).forEach(
+//     (key) => currentUrl[key] == null && delete currentUrl[key]
+//   );
 
-// REMOVE KEY FROM QUERY
-export function removeKeysFromQuery({
-  searchParams,
-  keysToRemove,
-}: RemoveUrlQueryParams) {
-  const currentUrl = qs.parse(searchParams);
+//   return `${window.location.pathname}?${qs.stringify(currentUrl)}`;
+// }
 
-  keysToRemove.forEach((key) => {
-    delete currentUrl[key];
-  });
+// // DEBOUNCE
+// export const debounce = (func: (...args: any[]) => void, delay: number) => {
+//   let timeoutId: NodeJS.Timeout | null;
+//   return (...args: any[]) => {
+//     if (timeoutId) clearTimeout(timeoutId);
+//     timeoutId = setTimeout(() => func.apply(null, args), delay);
+//   };
+// };
 
-  // Remove null or undefined values
-  Object.keys(currentUrl).forEach(
-    (key) => currentUrl[key] == null && delete currentUrl[key]
-  );
+// // GE IMAGE SIZE
+// export type AspectRatioKey = keyof typeof aspectRatioOptions;
+// export const getImageSize = (
+//   type: string,
+//   image: any,
+//   dimension: "width" | "height"
+// ): number => {
+//   if (type === "fill") {
+//     return (
+//       aspectRatioOptions[image.aspectRatio as AspectRatioKey]?.[dimension] ||
+//       1000
+//     );
+//   }
+//   return image?.[dimension] || 1000;
+// };
 
-  return `${window.location.pathname}?${qs.stringify(currentUrl)}`;
-}
+// // DOWNLOAD IMAGE
+// export const download = (url: string, filename: string) => {
+//   if (!url) {
+//     throw new Error("Resource URL not provided! You need to provide one");
+//   }
 
-// DEBOUNCE
-export const debounce = (func: (...args: any[]) => void, delay: number) => {
-  let timeoutId: NodeJS.Timeout | null;
-  return (...args: any[]) => {
-    if (timeoutId) clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => func.apply(null, args), delay);
-  };
-};
+//   fetch(url)
+//     .then((response) => response.blob())
+//     .then((blob) => {
+//       const blobURL = URL.createObjectURL(blob);
+//       const a = document.createElement("a");
+//       a.href = blobURL;
 
-// GE IMAGE SIZE
-export type AspectRatioKey = keyof typeof aspectRatioOptions;
-export const getImageSize = (
-  type: string,
-  image: any,
-  dimension: "width" | "height"
-): number => {
-  if (type === "fill") {
-    return (
-      aspectRatioOptions[image.aspectRatio as AspectRatioKey]?.[dimension] ||
-      1000
-    );
-  }
-  return image?.[dimension] || 1000;
-};
+//       if (filename && filename.length)
+//         a.download = `${filename.replace(" ", "_")}.png`;
+//       document.body.appendChild(a);
+//       a.click();
+//     })
+//     .catch((error) => console.log({ error }));
+// };
 
-// DOWNLOAD IMAGE
-export const download = (url: string, filename: string) => {
-  if (!url) {
-    throw new Error("Resource URL not provided! You need to provide one");
-  }
+// // DEEP MERGE OBJECTS
+// export const deepMergeObjects = (obj1: any, obj2: any) => {
+//   if(obj2 === null || obj2 === undefined) {
+//     return obj1;
+//   }
 
-  fetch(url)
-    .then((response) => response.blob())
-    .then((blob) => {
-      const blobURL = URL.createObjectURL(blob);
-      const a = document.createElement("a");
-      a.href = blobURL;
+//   let output = { ...obj2 };
 
-      if (filename && filename.length)
-        a.download = `${filename.replace(" ", "_")}.png`;
-      document.body.appendChild(a);
-      a.click();
-    })
-    .catch((error) => console.log({ error }));
-};
+//   for (let key in obj1) {
+//     if (obj1.hasOwnProperty(key)) {
+//       if (
+//         obj1[key] &&
+//         typeof obj1[key] === "object" &&
+//         obj2[key] &&
+//         typeof obj2[key] === "object"
+//       ) {
+//         output[key] = deepMergeObjects(obj1[key], obj2[key]);
+//       } else {
+//         output[key] = obj1[key];
+//       }
+//     }
+//   }
 
-// DEEP MERGE OBJECTS
-export const deepMergeObjects = (obj1: any, obj2: any) => {
-  if(obj2 === null || obj2 === undefined) {
-    return obj1;
-  }
-
-  let output = { ...obj2 };
-
-  for (let key in obj1) {
-    if (obj1.hasOwnProperty(key)) {
-      if (
-        obj1[key] &&
-        typeof obj1[key] === "object" &&
-        obj2[key] &&
-        typeof obj2[key] === "object"
-      ) {
-        output[key] = deepMergeObjects(obj1[key], obj2[key]);
-      } else {
-        output[key] = obj1[key];
-      }
-    }
-  }
-
-  return output;
-};
+//   return output;
+// };
